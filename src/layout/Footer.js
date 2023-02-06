8d3e8f0221263ca8e56554124c9c396a750bc858
Shane Keney <shane.keney@curaleaf.com>
2023-02-06 15:04:29 -0500

    chore: fixed footer elements & added personal info

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,19 +3,38 @@ const Footer = () => {
     <footer className="footer">
       <div className="copy">
         <p>E: sckeney@gmail.com</p>
-        <p>T: +1 (234) 567 80 98</p>
+        <p>T: +1 (860) 815 7873</p>
       </div>
       <div className="soc-box">
         <div className="follow-label">Follow Me</div>
         <div className="soc">
-          <a target="_blank" rel="noreferrer" href="https://www.pinterest.com/">
-            <span className="icon fab fa-pinterest" />
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/shane-keney/"
+          >
+            <span className="icon fab fa-linkedin" />
           </a>
-          <a target="_blank" rel="noreferrer" href="https://www.instagram.com/">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/ShaneKeney"
+          >
+            <span className="icon fab fa-github" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/skeney8/"
+          >
             <span className="icon fab fa-instagram" />
           </a>
-          <a target="_blank" rel="noreferrer" href="https://dribbble.com/">
-            <span className="icon fab fa-dribbble" />
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.pinterest.com/keney0601/"
+          >
+            <span className="icon fab fa-pinterest" />
           </a>
         </div>
       </div>
