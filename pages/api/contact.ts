73d27cc183450ad61a36e9df8662c302febb7548
Shane Keney <shane.keney@curaleaf.com>
2023-02-09 14:58:08 -0500

    feat: api route to handle sending email from potential contacts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,7 +2,62 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import sgMail from "@sendgrid/mail";
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
+
+interface SendGridEmail {
+  to: string;
+  from: string;
+  templateId: string;
+  dynamic_template_data: DyanamicTemplateData;
+}
+
+interface DyanamicTemplateData {
+  requester: string;
+  message: string;
+  requesterEmail: string;
+  mobile: string;
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).json({ name: "John Doe" });
+  const { name, email, message } = req.body;
+
+  if (!email || !validateEmail(email))
+    res.status(400).json({ error: "Please input valid email" });
+  if (!name) res.status(400).json({ error: "Name is required" });
+  if (!message) res.status(400).json({ error: "Message is required" });
+
+  const emailToSend: SendGridEmail = {
+    // Don't get confused...
+    // This is basically my personal email address for both as the API is handling sending on my behalf
+    to: process.env.CONTACT_EMAIL as string,
+    from: email,
+    templateId: "d-127d36b1c1e5415ab71893efe57fefa0",
+    dynamic_template_data: {
+      requester: req.body.name,
+      message: req.body.message,
+      requesterEmail: req.body.email,
+      mobile: req.body.mobile || "N/A",
+    },
+  };
+
+  return sgMail
+    .send(emailToSend)
+    .then(() => {
+      return res
+        .status(200)
+        .json({ success: true, message: "Email sent successfully" });
+    })
+    .catch((error) => {
+      console.log(JSON.stringify(error));
+      return res.status(500).json({
+        success: false,
+        errorCode: "INTERNAL_SERVER_ERROR",
+        message: "Error sending email to Shane Keney.  Please try again later.",
+      });
+    });
+}
+
+function validateEmail(email: string) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
 }
